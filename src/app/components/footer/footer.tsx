7fd328b97671843308de3028b049f47ebe1df3e1
Benjamin Wesson <benjamin.j.wesson@gmail.com>
2025-07-19 11:28:06 -0400

    new footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,13 +1,53 @@
-"use client";
-import styles from "./footer.module.css";
-
-
+import styles from './footer.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
 export default function Footer() {
-  return (
-    <div>Footer</div>
-  ) 
-}
+    return (
+        <div className={styles.grid_container}>
+            <div className={styles.card}>
+                <div className={styles.logoContainer}>
+                    <div><p className={styles.footerTitle}>Ben's Eats</p></div>
+                    <div><Image className={styles.logo} width={100} height={100} src="/logo.png" alt="Logo" /></div>
+                </div>
+                
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipiscing elit. 
+                    Quisque faucibus ex sapien vitae pellentesque sem placerat. 
+                    In id cursus mi pretium tellus duis convallis.
+                    Tempus leo eu aenean sed diam urna tempor.
 
+                </p>
+            </div>
+            <div className={styles.listCard}>
+                <div className={styles.list}>
+                    <p>Navigation</p>
+                    <ul>
+                        <li><Link href="/">Home</Link></li>
+                        <li><Link href="/about">About</Link></li>
+                        <li><Link href="/contact">Contact</Link></li>
+                    </ul>
+                </div>
+                <div className={styles.list}>
+                    <p>Categories</p>
+                    <ul>
+                        <li><Link href="/category/food">Food</Link></li>
+                        <li><Link href="/category/drinks">Drinks</Link></li>
+                        <li><Link href="/category/desserts">Desserts</Link></li>
+                    </ul>
+                </div>
+                <div className={styles.list}>
+                    <p>Socials</p>
+                    <ul>
+                        <li><Link href="/socials/facebook">Facebook</Link></li>
+                        <li><Link href="/socials/twitter">Twitter</Link></li>
+                        <li><Link href="/socials/instagram">Instagram</Link></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    );
+}
 
 
 
