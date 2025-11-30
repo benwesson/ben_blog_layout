7861210c82554234de50e6c46d81a02267f9c669
Benjamin Wesson <benjamin.j.wesson@gmail.com>
2025-11-30 04:53:12 -0500

    replaced recipe page by reusing categories component

--- src/app/(home)/recipes/page.tsx
+++ src/app/(home)/recipes/page.tsx
@@ -1,79 +1,11 @@
-import styles from "./recipes.module.css";
-import Link from "next/link";
-import Image from "next/image";
+import Categories from "@/components/categories/categories";
 
-export default function Categories() {
-  return (
-    <>
-      <div className={styles.categoryTitle}>Explore Recipes</div>
-      
-      <div className={styles.container}>
-        <div className={styles.category}>
-            
-        <Link href={"/breakfast"} >
-          <div>
-          <Image
-            className={styles.image}
-            src="/breakfast.jpg"
-            alt="breakfast"
-            width={300}
-            height={300}
-          />
-          Breakfast
-          </div>
-        </Link>
-        </div>
 
-        <div className={styles.category}>
-        <Link href={"/lunch"} >
-          <div>
-          <Image
-            className={styles.image}
-            src="/lunch.jpg"
-            alt="lunch"
-            width={300}
-            height={300}
-          />
-          Lunch
-          </div>
-        </Link>
-        </div>
-
-        <div className={styles.category}>
-        <Link href={"/dinner"} >
-          <div>
-          <Image
-            className={styles.image}
-            src="/dinner.jpg"
-            alt="dinner"
-            width={300}
-            height={300}
-          />
-          Dinner
-          </div>
-        </Link>
-        </div>
-
-        <div className={styles.category}>
-        <Link href={"/snacks"} >
-          <div>
-          <Image
-            className={styles.image}
-            src="/snacks.jpg"
-            alt="snacks"
-            width={300}
-            height={300}
-          />
-          Snacks
-          </div>
-        </Link>
-        </div>
-       
-      </div>
-    </>
-  );
+export default async function Recipes() {
+	return (
+	<Categories />
+	);
 }
-    
     
                 
       
